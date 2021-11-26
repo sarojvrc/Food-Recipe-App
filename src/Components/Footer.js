2ba43000d37a9b9082a11a5d2ebc64e84373c3df
sarojvrc <saroj.vrc@gmail.com>
2021-11-26 23:21:09 +0530

    footer added

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <footer>© Food App made by Saroj</footer>
+    </div>
+  );
+}
+
+export default Footer;
