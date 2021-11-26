64c3c849ebeb172f846d4729fc17a65ae2b09ba9
sarojvrc <saroj.vrc@gmail.com>
2021-11-26 23:00:00 +0530

    recipe updated

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ import RecipeTile from "./Components/RecipeTile";
 function App() {
   const [query, setQuery] = useState("");
   const [recipes, setRecipes] = useState([]);
+  const [healthLabel, sethealthLabel] = useState("vegan");
 
-  const url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&health=alcohol-free`;
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&health=${healthLabel}`;
 
   async function getRecipes() {
     const result = await axios.get(url);
@@ -34,6 +35,21 @@ function App() {
           placeholder="Enter Ingridint"
         />
         <input className="app_submit" type="submit" value="Search" />
+        <select className="app_healthlevel" >
+          <option onClick={() => sethealthLabel("vegan") }>Vegan</option>
+          <option onClick={() => sethealthLabel("wheat-free") }>Wheat-free</option>
+          <option onClick={() => sethealthLabel("vegetarian") }>Vegetarian</option>
+          <option onClick={() => sethealthLabel("tree-nut-free	") }>Tree-nut-free	</option>
+          <option onClick={() => sethealthLabel("sulfite-free") }>Sulfite-free</option>
+          <option onClick={() => sethealthLabel("sugar-conscious") }>Sugar-conscious</option>
+          <option onClick={() => sethealthLabel("soy-free") }>Soy-free</option>
+          <option onClick={() => sethealthLabel("shellfish-free") }>Shellfish-free</option>
+          <option onClick={() => sethealthLabel("egg-free") }>Egg-free</option>
+          <option onClick={() => sethealthLabel("fish-free") }>Fish-free</option>
+          <option onClick={() => sethealthLabel("fodmap-free") }>Fodmap-free</option>
+          <option onClick={() => sethealthLabel("gluten-free") }>Gluten-free</option>
+          <option onClick={() => sethealthLabel("alcohol-free") }>Alcohol-free</option>
+        </select>
       </form>
 
       <div className="app_recipes">
--- src/Components/RecipeTile.js
+++ src/Components/RecipeTile.js
@@ -3,7 +3,12 @@ import "./RecipeTile.css";
 
 function RecipeTile({ recipe }) {
   return (
-    <div className="RecipeTile">
+    <div
+      className="RecipeTile"
+      onClick={() => {
+        window.open(recipe["recipe"]["url"]);
+      }}
+    >
       <img src={recipe["recipe"]["image"]} className="recipetile_Image" />
       <p className="recipetile_name">{recipe["recipe"]["label"]}</p>
     </div>
